0f6146e786c35ffa34cf13baa46f61708f70fa38
xiyou <494949543@qq.com>
2022-08-03 08:43:10 +0800

    deploy

---
+++ precache-manifest.d3134e82e90bbf094bfc5aa6305e231a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53983584268708fd0767",
+    "url": "/money-website/css/app.8cafb1f5.css"
+  },
+  {
+    "revision": "3c52e7e8015e935daa691ee37bac68e6",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "53983584268708fd0767",
+    "url": "/money-website/js/app.522b72cf.js"
+  },
+  {
+    "revision": "ce24e63df9bc999b0ca6",
+    "url": "/money-website/js/chunk-vendors.7cb40f49.js"
+  },
+  {
+    "revision": "5a3bc337ed9627c080692ef3c952b5ba",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "86169823a36caf1b4cc2685e8b810c64",
+    "url": "/money-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
